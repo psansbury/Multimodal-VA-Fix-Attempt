486e4b9e6f0be2bb5385d4bbcb813e07986604d7
psansbury <psansbury@lenovo.com>
2025-12-16 11:12:43 +0000

    Update preBuild.bash

--- preBuild.bash
+++ preBuild.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This file contains bash commands that will be executed at the beginning of the container build process,
-# before any system packages or programming language specific package have been installed.
-#
-# Note: This file may be removed if you don't need to use it
+
+RUN python3 -m pip install --upgrade pip setuptools wheel && \
+    pip install --user --no-cache-dir --prefer-binary \
+      -r /opt/project/build/requirements.txt
